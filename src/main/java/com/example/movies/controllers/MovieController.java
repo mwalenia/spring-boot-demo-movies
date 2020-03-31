a7f2aa1aae47142905e75223b2120b73991897db
Michal Walenia <michal.walenia@polidea.com>
2020-03-31 13:12:59 +0200

    Remove unnecessary annotation

--- src/main/java/com/example/movies/controllers/MovieController.java
+++ src/main/java/com/example/movies/controllers/MovieController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MovieController {
   final MovieRepository repository;
 
-  @Autowired
   public MovieController(MovieRepository repository) {
     this.repository = repository;
   }
